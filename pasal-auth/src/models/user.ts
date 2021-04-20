80a6b9cd96e0f4fb506d6544349f6292d63adf6b
bharat kumar shah <akashche72@gmail.com>
2021-04-20 17:28:49 +0400

    Writing user model

---
+++ pasal-auth/src/models/user.ts
@@ -0,0 +1,75 @@
+import mongoose from "mongoose";
+import { Password } from "../services/password";
+
+enum usertype {
+  seller = "seller",
+  buyer = "buyer",
+}
+
+// Creating interface
+interface UserAttrs {
+  email: string;
+  password: string;
+  usertype: usertype;
+}
+
+// An interface descript the properties that a user document pass
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+  usertype: usertype;
+}
+
+// An interface define build function is available to the model
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// Create schema
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    usertype: {
+      type: String,
+      enum: Object.values(usertype),
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+// Before saving the document just hash the password
+userSchema.pre("save", async function (done) {
+  // Check if password is modified
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  // Call the done function
+  done();
+});
+
+// Build
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+// Create mongoose model
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+export { User };
