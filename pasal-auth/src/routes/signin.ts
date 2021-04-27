dfa465ba112a8ca3f888ac81020a7f4ff21fb9f9
bharat kumar shah <akashche72@gmail.com>
2021-04-27 18:24:46 +0400

    Working on current-user middleware

--- pasal-auth/src/routes/signin.ts
+++ pasal-auth/src/routes/signin.ts
@@ -1,5 +1,6 @@
 import express, {Request, Response} from 'express';
 import {body} from 'express-validator';
+
 const router = express.Router();
 
 router.get('/api/users/test', async(req:Request, res: Response) => {
